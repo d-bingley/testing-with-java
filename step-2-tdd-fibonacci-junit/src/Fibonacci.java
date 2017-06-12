c6f9d0d5d2703c31d0cc5def8a0692ee28987499
d-bingley <d_bingley@hotmail.co.uk>
2017-06-12 08:33:23 +0100

    Commit all files created so far

--- step-2-tdd-fibonacci-junit/src/Fibonacci.java
+++ step-2-tdd-fibonacci-junit/src/Fibonacci.java
@@ -1,2 +1,13 @@
 public class Fibonacci {
+public int calculate (int index){
+     if (index < 0) {
+         throw new IllegalArgumentException("Fibonacci indexes are defined only on non-negative integers.");
+    }
+
+    if (index == 0 || index == 1){
+        return 1;
+    }
+    return calculate(index-1)+ calculate(index -2);
+        //add previous two numbers
+}
 }
