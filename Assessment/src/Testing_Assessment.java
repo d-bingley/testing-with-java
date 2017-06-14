1bc8b2da22636db2f808b626ec7b571a686f70bd
d-bingley <d_bingley@hotmail.co.uk>
2017-06-14 09:51:08 +0100

    finding id section

---
+++ Assessment/src/Testing_Assessment.java
@@ -0,0 +1,204 @@
+package uk.org.adacollege.apprenticeship.testing;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.function.Function;
+
+public class SolutionIT {
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+    private static String startUrl;
+    private static String myWhipbirdsMenuId ="my-whipbirds-menu" ;
+    private static String aboutMenuId = "about-menu";
+    private static String logOutMenuId = "log-out-menu";
+    private static String logInMenuId = "log-in-menu";
+    private static String emailInputId = "email";
+    private static String passwordInputId = "password";
+    private static String validEmail = "daniella.bingley@adacollege.org.uk";
+    private static String invalidEmail = validEmail + ".nothing";
+    private static String validPassword = "whipit";
+    private static String invalidPassword = validPassword + "-invalid";
+    private static String logInButtonId = "login-button";
+    private static String logOutButtonId = "logout-button";
+    private static String popupMessageId = "popup-message";
+
+    // ========= UTILITY METHODS =========
+
+    /**
+     * Source & usage: https://stackoverflow.com/a/5709805
+     */
+    private static Function<WebDriver, WebElement> presenceOfElementLocated(final By locator) {
+        return new Function<WebDriver, WebElement>() {
+            @Override
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(locator);
+            }
+        };
+    }
+
+    private static void logIn(Boolean withValidCredentials) {
+        String email = withValidCredentials ? validEmail : invalidEmail;
+        String password = withValidCredentials ? validPassword : invalidPassword;
+
+        wait.until(presenceOfElementLocated(By.id(logInMenuId)));
+        driver.findElement(By.id(logInMenuId)).click();
+
+        wait.until(presenceOfElementLocated(By.id(emailInputId)));
+        driver.findElement(By.id(emailInputId)).sendKeys(email);
+
+        wait.until(presenceOfElementLocated(By.id(passwordInputId)));
+        driver.findElement(By.id(passwordInputId)).sendKeys(password);
+
+        wait.until(presenceOfElementLocated(By.id(logInButtonId)));
+        driver.findElement(By.id(logInButtonId)).click();
+
+        if (withValidCredentials) {
+            wait.until(new ExpectedCondition<Boolean>() {
+                public Boolean apply(WebDriver driver) {
+                    return driver.getTitle().equals("whipbird: my whipbirds");
+                }
+            });
+        }
+    }
+
+    private static void logOut() {
+        Boolean isLoggedIn = (driver.findElements(By.id(logOutMenuId)).size() > 0);
+
+        if (isLoggedIn) {
+            wait.until(presenceOfElementLocated(By.id(logOutMenuId)));
+            driver.findElement(By.id(logOutMenuId)).click();
+
+            wait.until(presenceOfElementLocated(By.id(logOutButtonId)));
+            driver.findElement(By.id(logOutButtonId)).click();
+        }
+    }
+
+    private static void assertElementPresent(String elementId) {
+        wait.until(presenceOfElementLocated(By.id(elementId)));
+        assertTrue(driver.findElements(By.id(elementId)).size() == 1);
+    }
+
+    private static void assertElementNotPresent(String elementId) {
+        assertTrue(driver.findElements(By.id(elementId)).size() == 0);
+    }
+
+    private static void assertTitleEquals(String expectedTitle) {
+        Boolean result = wait.until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver driver) {
+                return driver.getTitle().equals(expectedTitle);
+            }
+        });
+        assertTrue(result);
+    }
+
+    private static void assertUrlEquals(String expectedUrl) {
+        // TODO: implement this method
+        // - use assertTitleEquals() as an example pattern to follow
+        // - search the web for how to find the current URL with Selenium
+    }
+
+    private static void assertElementTextEquals(By selector, String expectedText) {
+        // TODO: implement this method
+        // - use assertTitleEquals() as an example pattern to follow
+        // - but instead of return driver.getTitle().equals(expectedTitle)
+        //   call driver.findElement() with the selector provided
+        //   and then get the text from that element
+        //   and then check that it equals the expected text
+    }
+
+    // ========= SCAFFOLDING =========
+
+    @BeforeClass
+    public static void beforeAll() {
+        startUrl = "http://whipbird.mattcalthrop.com/";
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 5);
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Before
+    public void beforeEach() {
+        driver.get(startUrl);
+    }
+
+    @After
+    public void afterEach() {
+        logOut();
+    }
+
+    // ========= TESTS =========
+
+    // --------- WHEN NOT LOGGED IN ---------
+
+    // Step 1
+    @Test
+    public void notLoggedIn_checkMenus() {
+        assertElementPresent(logInMenuId);
+        assertElementNotPresent(logOutMenuId);
+        // TODO: complete for all other menus
+    }
+
+    // Step 2
+    @Test
+    public void notLoggedIn_checkCurrentPage() {
+        // TODO
+    }
+
+    // Step 3
+    @Test
+    public void notLoggedIn_clickAboutMenu() {
+        // TODO
+    }
+
+    // Step 4
+    @Test
+    public void notLoggedIn_logInWithIncorrectCredentials() {
+        // TODO
+    }
+
+    // --------- WHEN LOGGED IN ---------
+
+    // Step 5
+    @Test
+    public void loggedIn_checkMenus() {
+        logIn(true);
+        // TODO
+    }
+
+    // Step 6
+    @Test
+    public void loggedIn_checkCurrentPage() {
+        // TODO
+    }
+
+    // Step 7
+    @Test
+    public void loggedIn_clickLogOutMenu() {
+        // TODO
+    }
+
+    // Step 8
+    @Test
+    public void loggedIn_addNewWhipbird() {
+        // TODO
+    }
+
+    // Step 9
+    @Test
+    public void loggedIn_addNewWhipbirdThenDeleteIt() {
+        // TODO
+    }
+}
